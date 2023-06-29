c281160485417c29c7e9e7ac91241fbbacf8734d
Paulo <pauloecomercial367@gmail.com>
2023-06-29 07:17:27 -0300

    :sparkles: Token JWT

---
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -0,0 +1,43 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.domain.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
+
+@Service
+public class TokenService {
+
+    @Value("{api.security.token.secret}")
+    private String secret;
+
+    // Metodo responável pela geração do token
+    public String gerarToken(Usuario usuario) {
+        try {
+            // É recomendado configurar uma data de expiração para tokens
+
+            // Algoritmo para fazer a assinatura digital do token
+            var algoritmo = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(usuario.getLogin())
+                    .withClaim("id", usuario.getId()) // podemos chamar esse método varias vezes para colocar mais info
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algoritmo);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar token jwt", exception);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+}
