3ecdb3f4dc5917d9d93bf4874993ab7a1b273a15
Paulo <pauloecomercial367@gmail.com>
2023-06-30 07:39:42 -0300

    :sparkles: Controle de acesso

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,7 +1,9 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +12,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * Classe responsável pela maneira que ocorrerá o "login" do usuário
@@ -19,23 +22,38 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity // Para dizer que iremos personalizar as configurações de segurança
 public class SecurityConfigurations {
 
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean // Para expor o retorno desse método, um objeto que pode ser injetado por alguma classe
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         // .csrf().disable(): Para desativar a proteção contra ataques csrf, o token jwt ja faz isso
         // .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS): Para dizer que iremos autenticar via Stateless
         // .and().build(): Para gerar o objeto SecurityFilterChain
+
+        // .and().authorizeRequests()
+        // .antMatchers(HttpMethod.POST, "/login").permitAll()
+        // Diz para o Spring que: Se vier uma requisição post para url "/login" libere, não é para checar se o usuário está logado
+        // .anyRequest().authenticated(): Diz para o Spring que qualquer outra url deve ser blockeada
+        // .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class): Diz para o spring que o meu filtro deve ser executado antes do dele
+
         return http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+                .and().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
-    @Bean // Serve para exportar uma classe para o Spring, fazendo com que ele consiga carregá-la e realze a injeção de dependência em outras classes
+    @Bean
+    // Serve para exportar uma classe para o Spring, fazendo com que ele consiga carregá-la e realze a injeção de dependência em outras classes
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,61 @@
+package med.voll.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+// OncePerRequestFilter: Faz a classe ser executada 1 vez por requisição
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Value("${api.security.token.secret}")
+    private String secret;
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UsuarioRepository repository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        System.out.println("CHAMANDO FILTER");
+        var tokenJWT = recuperarToken(request);
+        if (tokenJWT != null) {
+            var subject = tokenService.getSubject(tokenJWT);
+            var usuario = repository.findByLogin(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            System.out.println("ABACATE");
+        }
+        // Para chamar o próximo filtro e seguir o fluxo da aplicação
+        // PRECISA SER CHAMADA PARA CONTINUAR A EXECUÇÂO
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request) {
+        var authorizationHeader = request.getHeader("Authorization");
+
+        if (authorizationHeader != null) {
+            return authorizationHeader.replace("Bearer ", "");
+        }
+
+        return null;
+
+    }
+
+
+}
